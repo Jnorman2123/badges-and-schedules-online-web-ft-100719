742a337b834a2c01901bbca0e5373e7af7035f06
Jonathan Norman <jnorman2123@gmail.com>
2019-10-08 15:00:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,42 @@
-# Write your code here.
+def badge_maker(name)
+  p "Hello, my name is #{name}."
+end
+
+
+# def batch_badge_creator(names)
+#   badge_messages = []
+#   names.each do |name|
+#     badge_messages << "Hello, my name is #{name}."
+#   end
+#   badge_messages
+# end
+
+def batch_badge_creator(names)
+  names.collect do |name|
+    "Hello, my name is #{name}."
+  end
+end
+
+# def assign_rooms(names)
+#   room = 1
+#   room_assignments = []
+#   names.each do |name|
+#     room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
+#     room += 1
+#   end
+#   room_assignments
+# end
+
+def assign_rooms(names)
+  room = 0
+  names.collect do |name|
+    room += 1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
+
+def printer(names)
+  names.collect do |name|
+    p "#{batch_badge_creator(names)} #{assign_rooms(names)}"
+  end
+end
