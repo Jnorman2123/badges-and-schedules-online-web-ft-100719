240b2ff8da2b478e07979a364e04c6137afe3caf
Jonathan Norman <jnorman2123@gmail.com>
2019-10-08 17:51:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,19 +4,17 @@ end
 
 
 
-# def batch_badge_creator(names)
-#   name_badges = []
-#   names.each do |name|
-#     name_badges << "Hello, my name is #{name}."
-#   end
-#   name_badges
-# end
-
 def batch_badge_creator(names)
-  names.collect do |name|
-    "Hello, my name is #{name}."
+  names.each do |name|
+    p "Hello, my name is #{name}."
   end
 end
+
+# def batch_badge_creator(names)
+#   names.collect do |name|
+#     "Hello, my name is #{name}."
+#   end
+# end
 
 # def assign_rooms(names)
 #   room = 0
