f78903a3e92571e247477baed588a0c6242ddca9
Jonathan Norman <jnorman2123@gmail.com>
2019-10-08 18:18:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,39 +4,39 @@ end
 
 
 
-def batch_badge_creator(names)
-  name_badges = []
-  names.each do |name|
-    p name_badges << "Hello, my name is #{name}."
-  end
-  name_badges
-end
-
 # def batch_badge_creator(names)
-#   names.collect do |name|
-#     "Hello, my name is #{name}."
+#   name_badges = []
+#   names.each do |name|
+#     p name_badges << "Hello, my name is #{name}."
 #   end
+#   name_badges
 # end
 
-def assign_rooms(names)
-  room = 0
-  room_assignments = []
-  names.each do |name|
-    room += 1
-    room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
+def batch_badge_creator(names)
+  names.collect do |name|
+    "Hello, my name is #{name}."
   end
-  p room_assignments
 end
 
 # def assign_rooms(names)
 #   room = 0
-#   names.collect do |name|
+#   room_assignments = []
+#   names.each do |name|
 #     room += 1
-#     "Hello, #{name}! You'll be assigned to room #{room}!"
+#     room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
 #   end
+#   p room_assignments
 # end
 
+def assign_rooms(names)
+  room = 0
+  names.collect do |name|
+    room += 1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
+
 def printer(names)
-  p batch_badge_creator(names)
-  p assign_rooms(names)
+  p split(batch_badge_creator(names))
+  p split(assign_rooms(names))
 end
