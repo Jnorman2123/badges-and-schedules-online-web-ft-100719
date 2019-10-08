c055c11cd4b9eef0cea8e79807afb6ae98b0ee85
Jonathan Norman <jnorman2123@gmail.com>
2019-10-08 16:08:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,13 @@
 def badge_maker(name)
   p "Hello, my name is #{name}."
 end
+
+
+
+def batch_badge_creator(names)
+  name_badges = []
+  names.each do |name|
+    name_badges << "Hello, my name is #{name}!"
+  end
+  name_badges
+end 
