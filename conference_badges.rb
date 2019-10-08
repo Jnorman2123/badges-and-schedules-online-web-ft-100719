af5645e34230f45edcc4feeef48e6b6180aeea86
Jonathan Norman <jnorman2123@gmail.com>
2019-10-08 16:20:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,40 +4,41 @@ end
 
 
 
-def batch_badge_creator(names)
-  name_badges = []
-  names.each do |name|
-    name_badges << "Hello, my name is #{name}."
-  end
-  name_badges
-end
-
 # def batch_badge_creator(names)
-#   names.collect do |name|
-#     "Hello, my name is #{name}."
+#   name_badges = []
+#   names.each do |name|
+#     name_badges << "Hello, my name is #{name}."
 #   end
+#   name_badges
 # end
 
-def assign_rooms(names)
-  room = 0
-  room_assignments = []
-  names.each do |name|
-    room += 1
-    room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
+def batch_badge_creator(names)
+  names.collect do |name|
+    "Hello, my name is #{name}."
   end
-  room_assignments
 end
 
 # def assign_rooms(names)
 #   room = 0
-#   names.collect do |name|
+#   room_assignments = []
+#   names.each do |name|
 #     room += 1
-#     "Hello, #{name}! You'll be assigned to room #{room}!"
+#     room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
 #   end
+#   room_assignments
 # end
+
+def assign_rooms(names)
+  room = 0
+  names.collect do |name|
+    room += 1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
 
 def printer(names)
   names.collect do |name|
     p batch_badge_creator(name)
+    p assign_rooms(name)
   end
 end
